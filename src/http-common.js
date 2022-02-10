0d713e9cdc3af1bbf3cf41bf792c12b2fb103485
Natthaphat-63 <s6301012620120@kmutnb.ac.th>
2022-02-10 19:21:45 +0700

    change baseurl

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios"; // Promise based HTTP client RESTful API
 
 export default axios.create({
-    baseURL:"https://todolist-react-mysql.herokuapp.com/",
+    baseURL:"https://todolist-react-mysql.herokuapp.com/api/",
     headers: {
         "Content-type": "application/json"
     }
